e67b778ce6d7f093f62121fe2c3a4059f4a9d743
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2017-09-17 18:36:01 -0400

    added more ignore, updated client

--- client.go
+++ client.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	server := flag.String("s", "127.0.0.1", "server to connect")
 	port := flag.Int("p", 8089, "sever port to connect")
-	zpool := flag.String("-Z", "zones", "zpool to sync")
+	zpool := flag.String("Z", "zones", "zpool to sync")
 	flag.Parse()
 
 	// start up a zfs daemon
@@ -24,18 +24,34 @@ func main() {
 		// block
 	}
 
+	// start up rpc connection
 	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", *server, *port), grpc.WithInsecure())
-
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
+	// rpc client
 	c := pb.NewZsyncClient(conn)
-	response, err := c.GetFilesystems(context.Background(), &pb.Empty{})
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	fmt.Println(response)
+	// ask existence
+
+	for _, filesystem := range zfsD.Filesystems {
+		log.Println("Asking for existence of zfs filesystem: ", filesystem.Name)
+		// ask questions
+		response, err := c.Exists(context.Background(), &pb.Empty{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		if response.Name == nil {
+			log.Println("Server did not find your zfs filesystem.")
+		} else {
+			log.Println("Server found your zfs filesystem.")
+		}
+
+	}
+	/*
+
+
+	 */
 }
